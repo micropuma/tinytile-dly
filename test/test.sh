cfbf36d6f0e5a43781e65e867d8afd3d1e90e645
micropuma <micropuma@163.com>
2025-12-01 12:47:17 +0800

    [test] transform dialect pipeline

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,22 @@
+# a transform dialect pipeline
+tutorial-compiler \
+    example-transform.mlir \
+    -debug-only=slice-listener \
+    -debug-only=tile-and-fuse \
+    -o result.mlir \
+    2>&1 | tee report-transform.log
+
+# a convolution tiling pipeline
 tutorial-compiler \
     example.mlir \
     -debug-only=slice-listener \
     -debug-only=tile-and-fuse \
     -o result.mlir \
     2>&1 | tee report.log
+
+# experimental pipeline  
+# the tutorial dequant op supports tiling  
+# but don't support vectorization and bufferization yet  
+tutorial-compiler \
+    example-tutorial.mlir \
+    2>&1 | tee report-experiment.log
