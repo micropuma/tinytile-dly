e2921df01b408d2c5061e03bb8c64c439b1b5ca5
micropuma <micropuma@163.com>
2025-12-18 23:38:47 +0800

    [chore] environment support

--- config.sh
+++ config.sh
@@ -4,3 +4,4 @@ export HTTPS_PROXY=http://127.0.0.1:7890
 export https_proxy=http://127.0.0.1:7890
 
 export PATH=/home/douliyang/large/mlir-workspace/tinytile-dly/build/bin:$PATH
+export PATH=/home/douliyang/large/mlir-workspace/tinytile-dly/third_party/llvm/build/bin:$PATH
